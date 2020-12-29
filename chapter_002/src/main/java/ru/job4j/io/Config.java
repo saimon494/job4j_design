ff6c1d817c9b24d7e9c518f19609bf655ca9389e
saimon494 <saimon.494@gmail.com>
2020-12-30 00:59:22 +0300

    1. Читаем файл конфигурации [#408864]

---
+++ chapter_002/src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,49 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try {
+            BufferedReader read = new BufferedReader(new FileReader(path));
+            read.lines()
+                    .filter(r -> (!r.contains("#") && r.contains("=")))
+                    .forEach(line -> {
+                        String[] lines = line.split("=");
+                        values.put(lines[0], lines[1]);
+                    });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("app.properties"));
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./data/app1.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(
+                config.value("hibernate.connection.username"),
+                is("postgres")
+        );
+    }
+
+    @Test
+    public void whenPairWithComment() {
+        String path = "./data/app.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(
+                config.value("hibernate.connection.password"),
+                is("password")
+        );
+    }
+}
