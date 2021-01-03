a20fd64846ec322c70579b3395546c7789147594
saimon494 <saimon.494@gmail.com>
2021-01-04 01:21:00 +0300

    3.0. Тестирование IO [#408856]

--- chapter_002/src/test/java/ru/job4j/io/AnalysisTest.java
+++ chapter_002/src/test/java/ru/job4j/io/AnalysisTest.java
@@ -1,6 +1,8 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.*;
 
@@ -10,6 +12,9 @@ import static org.junit.Assert.*;
 public class AnalysisTest {
     String source = "./data/server.log";
     String target = "./data/unavailable.csv";
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Test
     public void analysisServerLog() throws IOException {
@@ -23,6 +28,27 @@ public class AnalysisTest {
         }
         Analysis analysis = new Analysis();
         analysis.unavailable(source, target);
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(target))) {
+            reader.lines().forEach(rsl::append);
+        }
+        assertThat(rsl.toString(), is("10:57:01 - 10:59:01; 11:01:02 - 12:02:02; "));
+    }
+
+    @Test
+    public void analysisServerLogTempFolder() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("200 10:59:01");
+            out.println("500 11:01:02");
+            out.println("200 12:02:02");
+        }
+        Analysis analysis = new Analysis();
+        analysis.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
         StringBuilder rsl = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(target))) {
             reader.lines().forEach(rsl::append);
