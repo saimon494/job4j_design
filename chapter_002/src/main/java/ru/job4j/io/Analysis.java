6fef9117d1b5f9862486bd70c937b5798eb999ab
saimon494 <saimon.494@gmail.com>
2021-01-03 21:43:27 +0300

    2. Анализ доступности сервера. [#408865]

---
+++ chapter_002/src/main/java/ru/job4j/io/Analysis.java
@@ -0,0 +1,26 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class Analysis {
+    public void unavailable(String source, String target) {
+        try (BufferedReader in = new BufferedReader(new FileReader(source));
+             PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = in.readLine()) != null) {
+                String[] lines = line.split(" ");
+                if (sb.length() == 0 && (lines[0].equals("400") || lines[0].equals("500"))) {
+                    sb.append(lines[1]).append(" - ");
+                }
+                if (sb.length() != 0 && (lines[0].equals("200") || lines[0].equals("300"))) {
+                    sb.append(lines[1]).append("; ");
+                    out.println(sb);
+                    sb = new StringBuilder();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
