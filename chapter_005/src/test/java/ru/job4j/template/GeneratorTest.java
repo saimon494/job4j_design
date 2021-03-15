0e463532b0c87c2b235e5e72504d2a1fa1d6c194
saimon494 <saimon.494@gmail.com>
2021-03-15 15:22:45 +0300

    3. Шаблонизатор. [#408885]

---
+++ chapter_005/src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,49 @@
+package ru.job4j.template;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.core.Is.is;
+
+public class GeneratorTest {
+
+    @Test
+    public void whenCorrectArgs() {
+        String template = "I am ${name}. Who are ${subject}?";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Ivan");
+        map.put("subject", "you");
+        Generator generator = new TextGenerator();
+        String result = generator.produce(template, map);
+        String expected = "I am Ivan. Who are you?";
+        assertThat(result, is(expected));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenIncorrectArgs() {
+        String template = "I am ${name}. Who are ${subject}?";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Ivan");
+        map.put("subject", "23");
+        Generator generator = new TextGenerator();
+        String result = generator.produce(template, map);
+        String expected = "I am Ivan. Who are you?";
+        assertThat(result, is(expected));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenIncorrectTemplate() {
+        String template = "I am ${name}. I am from ${subject}.";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Ivan");
+        map.put("subject", "you");
+        Generator generator = new TextGenerator();
+        String result = generator.produce(template, map);
+        String expected = "I am Ivan. Who are you?";
+        assertThat(result, is(expected));
+    }
+
+}
