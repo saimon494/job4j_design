18820a7ac6f6d4143bc075d18df835d2f5c39f72
saimon494 <saimon.494@gmail.com>
2021-03-15 10:40:17 +0300

    Генератор текста, JMH, исправление

--- chapter_004/src/main/java/ru/job4j/gc/Article.java
+++ chapter_004/src/main/java/ru/job4j/gc/Article.java
@@ -7,7 +7,32 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Benchmark)
+@Fork(value = 1, warmups = 1)
+@Warmup(iterations = 5)
+@Measurement(iterations = 5)
+
 public class Article {
+
+    private final String originStr = "Мой дядя самых честных правил, "
+            + "Когда не в шутку занемог, "
+            + "Он уважать себя заставил "
+            + "И лучше выдумать не мог. "
+            + "Его пример другим наука; "
+            + "Но, боже мой, какая скука "
+            + "С больным сидеть и день и ночь, "
+            + "Не отходя ни шагу прочь! "
+            + "Какое низкое коварство "
+            + "Полуживого забавлять, "
+            + "Ему подушки поправлять, "
+            + "Печально подносить лекарство, "
+            + "Вздыхать и думать про себя: "
+            + "Когда же черт возьмет тебя!";
+
+    private final String testStr = "Мой дядя мог думать про тебя и день и ночь";
+
     public static boolean generateBy(String origin, String line) {
         boolean rsl = true;
         String[] originList = origin.split("[^а-яА-Яa-zA-Z]");
@@ -25,18 +50,30 @@ public class Article {
         return rsl;
     }
 
-    @Benchmark
-    @Fork(value = 1, warmups = 1)
-    @Warmup(iterations = 5)
-    @BenchmarkMode(Mode.AverageTime)
-    @OutputTimeUnit(TimeUnit.NANOSECONDS)
-    public void init() {
-        // Do nothing
+    public static boolean generateByList(String origin, String line) {
+        boolean rsl = true;
+        String[] originList = origin.split("[^а-яА-Яa-zA-Z]");
+        String[] lineList = line.split("[^а-яА-Яa-zA-Z]");
+        for (String s : lineList) {
+            rsl = Arrays.asList(originList).contains(s);
+            if (!rsl) {
+                break;
+            }
+        }
+        return rsl;
     }
 
-    public static void main(String[] args) {
-        String origin = "Когда не в шутку занемог, ";
-        String[] originList = origin.split("[^а-яА-Я]");
-        System.out.println(Arrays.toString(originList));
+    @Benchmark
+    public void testByMap() {
+        generateBy(originStr, testStr);
+    }
+
+    @Benchmark
+    public void testByList() {
+        generateByList(originStr, testStr);
+    }
+
+    public static void main(String[] args) throws Exception {
+        org.openjdk.jmh.Main.main(args);
     }
 }
