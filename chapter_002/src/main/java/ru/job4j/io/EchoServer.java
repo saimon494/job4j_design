091edd214a3cfcdb465f28be32584c92562f5811
saimon494 <saimon.494@gmail.com>
2021-09-22 21:56:51 +0300

    EchoServer edit

--- chapter_002/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_002/src/main/java/ru/job4j/io/EchoServer.java
@@ -4,43 +4,50 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-    private static final Logger LOG = LoggerFactory.getLogger(EchoServer.class.getName());
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        try (ServerSocket server = new ServerSocket(9000)) {
-            while (true) {
-                Socket socket = server.accept();
-                try (OutputStream out = socket.getOutputStream();
-                     BufferedReader in = new BufferedReader(
-                             new InputStreamReader(socket.getInputStream()))) {
-                    String str = in.readLine();
-                    String answer = "";
-                    while (!str.isEmpty()) {
-                        if (str.contains("Hello")) {
-                            answer = "Hello, dear friend!";
-                        } else if (str.contains("Exit")) {
-                            answer = "Bye!";
-                            server.close();
-                        } else if (str.contains("msg=")) {
-                            String[] tmp = str.split("[ |=]");
-                            answer = tmp[2];
+        try {
+            try (ServerSocket server = new ServerSocket(9000)) {
+                while (true) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream();
+                         BufferedReader in = new BufferedReader(
+                                 new InputStreamReader(socket.getInputStream()))) {
+                        boolean isStop = false;
+                        String answer = "";
+                        String str = in.readLine().toLowerCase();
+                        while (!str.isEmpty()) {
+                            System.out.println(str);
+                            if (str.contains("msg=")) {
+                                if (str.contains("exit")) {
+                                    isStop = true;
+                                }
+                                if (str.contains("hello")) {
+                                    answer = "Hello, dear friend!";
+                                } else {
+                                    answer = str.split("=")[1].split(" ")[0];
+                                }
+                            }
+                            str = in.readLine();
                         }
-                        System.out.println(str);
-                    }
-                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    if (!answer.isEmpty()) {
-                        out.write((answer + "\r\n").getBytes());
+                        if (isStop) {
+                            break;
+                        }
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        out.write(answer.getBytes());
                     }
                 }
             }
-        } catch (Exception e) {
-            LOG.error("IOException", e);
+        } catch (IOException e) {
+            LOG.error("An exception has occurred: ", e);
         }
     }
 }
